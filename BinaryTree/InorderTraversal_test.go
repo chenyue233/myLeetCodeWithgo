cc7fc852fe14854a452795bc0e1bd2e51e7feb0d
chenyue233 <1269954795@qq.com>
2020-02-27 11:17:27 +0800

    添加测试

--- BinaryTree/InorderTraversal_test.go
+++ BinaryTree/InorderTraversal_test.go
@@ -2,41 +2,21 @@ package binarytree
 
 import "testing"
 
-type question struct {
-	para
-	ans
-}
-
-// para 是参数
-type para struct {
-	pre []int
-	in  []int
-}
-
-// ans 是答案
-type ans struct {
-	one []int
-}
-
-func TestinorderTraversal(t *testing.T) {
-	testArray := []question{
-		question{
-			para{
-				[]int{1, 2, 3},
-				[]int{1, 3, 2},
-			},
-			ans{
-				[]int{1, 3, 2}
-			},
-		},
+func TestInorderTraversal(t *testing.T) {
+	testNode := TreeNode{1, &TreeNode{2, nil, nil}, &TreeNode{3, nil, nil}}
+	expected := [...]int{2, 1, 3}
+	res := inorderTraversal(&testNode)
+	if len(res) != len(expected) {
+		t.Errorf("len(res)!= len(expected),expected '%d',but got '%d'", expected, res)
 	}
-	for _, value := range testArray {
-		resOfMyFunc := inorderTraversal(value[0])
-		if resOfMyFunc != expected[index] {
-			t.Errorf("expected '%d',but got '%d'", expected[index], resOfMyFunc)
-			
+	if (&expected == nil) != (res == nil) {
+		t.Errorf("if (&expected == nil) != (res == nil),expected '%d',but got '%d'", expected, res)
+	}
+	// 防止数组越界
+	ress := res[:len(expected)]
+	for index, value := range expected {
+		if value != ress[index] {
+			t.Errorf("expected '%d',but got '%d'", expected, res)
 		}
-
 	}
-
 }
