3c1b7f908c624dd6bb393f67122524a84df64949
chenyue233 <1269954795@qq.com>
2020-03-03 15:08:47 +0800

    100相同的树 104二叉树最大深度

---
+++ BinaryTree/100.相同的树.go
@@ -0,0 +1,28 @@
+package binarytree
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if (p == nil) && (q == nil) {
+		return true
+	}
+	if p == nil && q != nil {
+		return false
+	}
+	if p != nil && q == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	res := isSameTree(p.Left, q.Left)
+	ress := isSameTree(p.Right, q.Right)
+	return res && ress
+}
---
+++ BinaryTree/104.二叉树的最大深度.go
@@ -0,0 +1,55 @@
+package binarytree
+
+/*
+ * @lc app=leetcode.cn id=104 lang=golang
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftint := maxDepth(root.Left)
+	Rightint := maxDepth(root.Right)
+	maxint := func(left, right int) int {
+		if left > right {
+			return left + 1
+		}
+		return right + 1
+	}
+	return maxint(leftint, Rightint)
+
+}
+
+// @lc code=end
+func maxDepthh(root *TreeNode) int {
+	var stack []*TreeNode
+	stack = append(stack, root)
+	res := 0
+	for len(stack) > 0 {
+		for i := 0; i < len(stack); i++ {
+			node := stack[0]
+			stack = stack[1:]
+			if &node.Left != nil {
+				stack = append(stack, node.Left)
+			}
+			if &node.Right != nil {
+				stack = append(stack, node.Right)
+			}
+		}
+		res++
+
+	}
+	return res
+}
