d5dd539bc85b5b0c750832c8aab179998f58587b
chenyue233 <1269954795@qq.com>
2020-03-04 14:43:45 +0800

    21合并两个有序列表

---
+++ list/21.合并两个有序链表.go
@@ -0,0 +1,39 @@
+package list
+
+/*
+ * @lc app=leetcode.cn id=21 lang=golang
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	pre := new(ListNode)
+	cur := pre
+	for (l1 != nil) && (l2 != nil) {
+		if l1.Val <= l2.Val {
+			cur.Next = l1
+			l1 = l1.Next
+		} else {
+			cur.Next = l2
+			l2 = l2.Next
+		}
+		cur = cur.Next
+	}
+	if l1 == nil && l2 != nil {
+		cur.Next = l2
+	} else if l2 == nil && l1 != nil {
+		cur.Next = l1
+	}
+
+	return pre.Next
+}
+
+// @lc code=end
